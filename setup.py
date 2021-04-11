a991d56120b83cd865f5377582903e53cb12c56a
marcos <marcosas.soares2@gmail.com>
2021-04-10 23:00:18 -0300

    Consertado bug pypi

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "pytools"
+PACKAGE = "pytools123456789"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Marcos A Soares"
@@ -110,7 +110,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('README.md'),
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
